f86bef5fa3d863bbb02ee048c2be3930ce9e32a2
accidentlywoo <accidentlywoo@gmail.com>
2021-04-09 22:19:56 +0900

    docs : 알고리즘 2주 차 살려줘라

--- QuakQuak/Programmers/Lottos.py
+++ QuakQuak/Programmers/Lottos.py
@@ -1,6 +1,14 @@
-def solustion(lottos, win_nums):
-    print(lottos.index())
+# def solustion(lottos, win_nums):
+#     print(lottos.index())
+#
+# solustion([1,2],[1,2])
+# a, b =1
+# print(a,b)
 
-solustion([1,2],[1,2])
-a, b =1
-print(a,b)
+a ='1 2 4'
+b = a.split(' ')
+print(b.index('4'))
+a = []
+a.append('a')
+a.append('b')
+print(a)
